7d6cf9d6508ba15d13d0964740a5fe76416fa1a2
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2022-10-20 15:02:01 -0500

    added format for missing zeros

--- day_2_tip-calculator/main.py
+++ day_2_tip-calculator/main.py
@@ -3,6 +3,8 @@ percentage = input("What percentage tip would you like to give? 10, 12, 15?")
 people = int(input("How many people to split the bill?"))
 
 percent = (float(percentage) / 100) + 1
+amount = round((total_bill / people) * percent, 2)
+final_amount = "{:.2f}".format(amount)
 
 
-print(f"Each person should pay: ${round((total_bill / people) * percent, 2)}")
+print(f"Each person should pay: ${final_amount}")
