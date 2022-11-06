ef2a0d17d536356ce6b049cbd3967b9f69f443e6
PrettyScript <JessicaDelcy@gmail.com>
2022-11-06 09:36:50 -0600

    created todos for high low game

---
+++ day_14_high_lower_game/main.py
@@ -0,0 +1,7 @@
+#TODO print logos for game
+#TODO Have a way to track user's score 
+#TODO Take in the user's answer and it is displayed as they progress in the game
+#TODO Create a function that will loop through the game data and provide the two comparisons
+    #TODO First time Compare A and Compare B and if correct, Compare B will then shift to Compare A and a new comparison will be in Compare B
+    #TODO If user is correct keep shifting, if not then they lose and their final score is displayed
+    #TODO The second logo is removed
