796c8618f70aed19b958a46332258d074375d099
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2022-11-08 13:26:39 -0600

    completed day 15 challenge

--- day_15_coffee_machine/main.py
+++ day_15_coffee_machine/main.py
@@ -5,16 +5,14 @@ def report():
     money = 0
     water = resources['water']
     milk = resources['milk']
-    coffee = resources['coffee']
-    resources = [water, milk, coffee, money]
+    coffee = resources['coffee']    
+
     print(f"Water: {water}ml")
     print(f"Milk: {milk}ml")
     print(f"Coffee: {coffee}g")
     print(f"Money: ${money}")
 
-    return resources
-
-# report()
+report()
 
 #TODO Print coffee prices
 def prices():
@@ -50,7 +48,7 @@ def get_coffee_price(coffee):
 
 #TODO Process Coins
 def process_coins():
-    # call check resources then process 
+    prices()
     print("Please insert coins.")
     quarters = int(input("how many quarters?: "))
     dimes = int(input("how many dimes?: "))
@@ -74,19 +72,41 @@ def check_transaction(coffee):
            refund = get_coffee_price(coffee) - coins
            refund = round(abs(refund), 2)
            print(f"Here is ${refund} dollars in change.")
-        print("Make coffee")
+           make_coffee(coffee)
+        make_coffee(coffee)
     else:
         print("Sorry that's not enough money. Money refunded.")
 
 # check_transaction('espresso')
 
 #TODO Make Coffee
-coffee = '☕'
 def make_coffee(coffee):
-    resources = report()
-    print(resources)
+    money = get_coffee_price(coffee)
 
-make_coffee('espresso')
+    if coffee == 'espresso':
+        resources['water'] = 300 - 50
+        resources['milk'] = 200 -0
+        resources['coffee'] = 100 -18
+        print("Here is your espresso☕. Enjoy!")
+        return True
+    elif coffee == 'latte':
+        resources['water'] = 300 - 200
+        resources['milk'] = 200 -150
+        resources['coffee'] = 100 - 24  
+        print("Here is your latte☕. Enjoy!")  
+        return True    
+    elif coffee == 'cappuccino':
+        resources['water'] = 300 - 250
+        resources['milk'] = 200 - 100
+        resources['coffee'] = 100 - 24       
+        print("Here is your cappuccino☕. Enjoy!")
+        return True
+
+    
+
+# make_coffee('espresso')
+
+
 #TODO Turn off coffee machine
 # def turn_off_machine(request):
 #     '''Takes user input to end code execution'''
@@ -98,11 +118,22 @@ def coffee_machine_on():
     if order == 'report':
         report()
     elif order == 'espresso':
-        process_coins()
+        if check_resources("espresso") == True:
+            check_transaction("espresso")
+        else:
+            print("Sorry there is not enough resources")
     elif order == 'latte':
-        process_coins()
+        if check_resources("latte") == True:
+            check_transaction("latte")
+        else:
+            print("Sorry there is not enough resources")            
     elif order == 'cappuccino':
-        process_coins()
+        if check_resources("cappuccino") == True:
+            check_transaction("cappuccino")
+        else:
+            print("Sorry there is not enough resources")            
     # elif order == 'off':
     #     turn_off_machine()
+
+coffee_machine_on()
     
