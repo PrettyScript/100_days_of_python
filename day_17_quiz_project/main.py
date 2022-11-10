a503f6a938862d41b2d23e04cc414715de60d24c
PrettyScript <JessicaDelcy@gmail.com>
2022-11-09 22:11:33 -0600

    created next question method

--- day_17_quiz_project/main.py
+++ day_17_quiz_project/main.py
@@ -1,10 +1,12 @@
 from question_model import Question 
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
-
 for question in question_data:
     questions = Question(question['text'], question['answer'])
     question_bank.append(questions)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+while #if quiz still has questions remaining
+quiz.next_question()
