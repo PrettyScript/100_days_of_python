f681c2dd64493116cc49a568df38c069bb48ae0c
PrettyScript <JessicaDelcy@gmail.com>
2022-11-10 14:27:21 -0600

    created dashed line

--- day_18_turtle/main.py
+++ day_18_turtle/main.py
@@ -4,9 +4,15 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("#957DAD")
 
-for _ in range(4):
-    tim.forward(200)
-    tim.right(90)
+# for _ in range(4):
+#     tim.forward(200)
+#     tim.right(90)
+
+for _ in range(20):
+    tim.pendown()
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
 
 
 
