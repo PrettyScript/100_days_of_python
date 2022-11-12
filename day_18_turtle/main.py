9f175df74fc496b3db7cb1b2dd5002f7607c15ea
PrettyScript <JessicaDelcy@gmail.com>
2022-11-12 10:16:46 -0600

    created color list

--- day_18_turtle/main.py
+++ day_18_turtle/main.py
@@ -1,9 +1,21 @@
-import colorgram
-colors = colorgram.extract('image.jpg', 10)
-print(colors[0].rgb.r)
-extracted_colors = colors
+# import colorgram
+# colors = colorgram.extract('image.jpg', 30)
+# rgb_colors = []
 
-for color in extracted_colors:
-    print(extracted_colors[color])
+# # for color in range(len(colors)):
+# #     r = colors[color].rgb.r
+# #     g = colors[color].rgb.g
+# #     b = colors[color].rgb.b
+# #     rgb_colors.append((r,g,b))
 
-# hirst_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+color_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+
