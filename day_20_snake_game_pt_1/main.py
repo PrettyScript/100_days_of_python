50444c8ed72490b4729084a96be0e045af10dfad
PrettyScript <JessicaDelcy@gmail.com>
2022-11-13 02:24:01 -0600

    created one snake for snake game

--- day_20_snake_game_pt_1/main.py
+++ day_20_snake_game_pt_1/main.py
@@ -6,12 +6,40 @@
 #TODO Detect collision with wall
 #TODO Detect collision with tail
 
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle(shape='square')
+    new_segment.color('white')
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for segment_num in range(len(segments) - 1, 0, -1):
+        # second to last segment
+        new_x = segments[segment_num - 1].xcor()
+        new_y = segments[segment_num - 1].ycor()
+        # last segment
+        segments[segment_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    # segments[0].left(90)
 
 
 
