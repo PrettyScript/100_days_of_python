ff7c0c21cdb1169f6c361b2182347b7a4b7dfb10
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2022-11-13 20:18:26 -0600

    completed day 21 snake game part 2

--- day_20_snake_game_pt_1/main.py
+++ day_20_snake_game_pt_1/main.py
@@ -8,6 +8,7 @@
 
 import time
 from turtle import Screen
+from typing import final
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
@@ -20,7 +21,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -36,10 +37,22 @@ while game_is_on:
     
     snake.move()
 
-    #Detect collison with food.
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.score()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
