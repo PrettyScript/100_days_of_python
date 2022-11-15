af572b3f39a65c06c16459fcb9fb122370b1efd7
PrettyScript <JessicaDelcy@gmail.com>
2022-11-15 02:36:18 -0600

    created ball class

--- day_22_build_pong/main.py
+++ day_22_build_pong/main.py
@@ -9,6 +9,8 @@
 
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -19,6 +21,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -28,7 +31,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
