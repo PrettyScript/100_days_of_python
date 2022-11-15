cbb07baa7674a390a225f19b03ff1b5ed15617f8
PrettyScript <JessicaDelcy@gmail.com>
2022-11-15 00:38:27 -0600

    created paddle class and refactored main.py file

--- day_22_build_pong/main.py
+++ day_22_build_pong/main.py
@@ -1,39 +1,34 @@
 #TODO Create the screen
-#TODO Create and move the a paddle
+#TODO Create and go the a paddle
 #TODO Create another paddle
-#TODO Create the ball and make it move
+#TODO Create the ball and make it go
 #TODO Detect collision with wall and bounce
 #TODO Detect collision with paddle
 #TODO Detect when paddle misses
 #TODO Keep Score
 
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Ping Pong")
+screen.tracer(0)
 
 
-turtle = Turtle()
-turtle.color("white")
-turtle.shape("square")
-# turtle.shapesize(stretch_wid=20, stretch_len=60)
-turtle.turtlesize(stretch_wid=5, stretch_len=1)
-turtle.penup()
-turtle.setpos(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
-
-def move_up():
-    turtle.goto(350, 5)
-
-def move_down():
-    turtle.goto(350, -5)
-
-screen.listen(move_up, "Up")
-screen.listen(move_down, "Down")
-
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
