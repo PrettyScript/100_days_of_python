a66fa6f0338c7042ef220a7b2c68e6530d10749c
PrettyScript <JessicaDelcy@gmail.com>
2022-11-19 16:23:07 -0600

    completed day 25 exercise 2

--- day_25_US_States_Game/main.py
+++ day_25_US_States_Game/main.py
@@ -17,5 +17,41 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data))
+# print(type(data))
 # print(data["temp"])
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data['temp'].to_list()
+# print(temp_list)
+# # avg_temp = sum(temp_list) / len(temp_list)
+# # print(f"Average temp: {round(avg_temp)}")
+
+# print(data['temp'].mean())
+# print(data['temp'].max())
+
+# # Get Data in Columns
+# print(data['condition'])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == 'Monday']
+# print(monday.condition)
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+print(celsius_to_fahrenheit(monday.temp))
+
+# Create a dataframe from scratch
+student_scores = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(student_scores)
+# print(data)
+data.to_csv("new_data.csv")
