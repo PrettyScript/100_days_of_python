497dee7e92b284efcba3d0c382a219548b6dfc8b
PrettyScript <JessicaDelcy@gmail.com>
2022-11-20 23:34:45 -0600

    completed day 25 US States Game

--- day_25_US_States_Game/main.py
+++ day_25_US_States_Game/main.py
@@ -14,21 +14,24 @@ states = state_data.state.tolist()
 val = state_data[state_data.state == "Florida"].iloc[0]["x_y_coordinates"]
 # print(val)
 
-
-# print(states)
 guessed_states = []
 
-
-quiz_on = True
-# TODO When user gets answer correct the name is placed on respective state
+# # TODO When user gets answer correct the name is placed on respective state
 
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(
-        title=f"{len(guessed_states)}/50 Correct", prompt="What's another state's name?"
+        title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?"
     ).title()
 
     if answer_state == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in states:
         guessed_states.append(answer_state)
@@ -39,14 +42,9 @@ while len(guessed_states) < 50:
         # state.goto(val)
         data = state_data[state_data.state == answer_state]
         state.goto(int(data.x), int(data.y))
-        state.write(data.state.item())  # Another way to get the raw data
+        # state.write(data.state.item())  # Another way to get the raw data
         state.write(answer_state)
         # answer_state = screen.textinput(title=f"{score}/50 Correct", prompt="What's another state's name?")
-    else:
-        quiz_on = False
-        turtle.write("You Lose!")
-
-# states_to_learn.csv
 
 
 screen.exitonclick()
