7ade84585355fee1c438a10fa5964744371471a6
PrettyScript <JessicaDelcy@gmail.com>
2022-11-22 23:17:10 -0600

    created tkinter labels

--- day_27_mile_to_kilometers_converter/tkinter/main.py
+++ day_27_mile_to_kilometers_converter/tkinter/main.py
@@ -5,10 +5,9 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
-
-
-
-
+# Label
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold "))
+my_label.pack()
 
 
 window.mainloop()
