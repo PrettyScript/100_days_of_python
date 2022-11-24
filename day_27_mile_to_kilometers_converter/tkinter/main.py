ea925dd61aeac969b607562e07e3ddd802152d2c
PrettyScript <JessicaDelcy@gmail.com>
2022-11-24 00:07:10 -0600

    created args, kw args and created entry class and button class

--- day_27_mile_to_kilometers_converter/tkinter/main.py
+++ day_27_mile_to_kilometers_converter/tkinter/main.py
@@ -1,13 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
 # Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold "))
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold "))
 my_label.pack()
 
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+# Entry (Input)
+
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
--- day_27_mile_to_kilometers_converter/tkinter/playground.py
+++ day_27_mile_to_kilometers_converter/tkinter/playground.py
@@ -1,7 +1,35 @@
 def add(*args):
+    print(args[1])
+
+    sum = 0
     for n in args:
-        n += n
+        sum += n
+    return sum
+
+
+print(add(3, 4, 2, 1, 7, 4, 3))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
     print(n)
 
 
-add(1, 2, 3)
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        # the get method is beneficial because if the
+        # value doesn't exist than you will
+        # receive a 'none' error
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
