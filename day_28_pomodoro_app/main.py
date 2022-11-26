e6154807a98c91aa7c25bfaca6de55b593b07e55
PrettyScript <JessicaDelcy@gmail.com>
2022-11-26 10:01:08 -0600

    created pomodoro ui completed

--- day_28_pomodoro_app/main.py
+++ day_28_pomodoro_app/main.py
@@ -25,8 +25,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-canvas.pack()
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(text="✓", bg=YELLOW, fg=GREEN, font=FONT_NAME)
+checkmark_label.grid(column=1, row=3)
 
 
 window.mainloop()
