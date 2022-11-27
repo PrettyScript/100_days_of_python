0ba0a0854b1e382c2089e425c01263139228214f
PrettyScript <JessicaDelcy@gmail.com>
2022-11-27 10:10:39 -0600

    completed day 29 project

--- day_29_password_manager/main.py
+++ day_29_password_manager/main.py
@@ -1,18 +1,125 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    # for char in range(nr_letters):
+    #   password_list.append(random.choice(letters))
+
+    # for char in range(nr_symbols):
+    #   password_list += random.choice(symbols)
+
+    # for char in range(nr_numbers):
+    #   password_list += random.choice(numbers)
+
+    password_list += [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    # for char in password_list:
+    #     password += char
+
+    # print(f"Your password is: {password}")
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def reset():
     website_input.delete(0, END)
     password_input.delete(0, END)
-    print("reset")
-    
+
+
 def save():
-    with open("data.txt", mode="a") as data:
-        data.write(f"{website_input.get()} | {email_input.get()} | {password_input.get()}\n")
-        
-    reset()
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    print(len(password))
+
+    # if len(website) == 0 or len(password):
+    #     messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    # else:
+    is_ok = messagebox.askokcancel(
+        title=website,
+        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?",
+    )
+
+    if is_ok:
+        with open("data.txt", mode="a") as data:
+            data.write(f"{website} | {email} | {password}\n")
+
+        reset()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -41,7 +148,7 @@ password_label.grid(column=0, row=3)
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36, command=save)
