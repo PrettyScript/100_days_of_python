b4d17f0c0a7d25d4a8c92ce65aaa46f265f8e5d8
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2022-11-26 18:00:16 -0600

    created UI for password manager

--- day_29_password_manager/main.py
+++ day_29_password_manager/main.py
@@ -11,8 +11,30 @@ window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=lock_img)
-canvas.grid(column=1, row=1)
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_input = Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 
 window.mainloop()
