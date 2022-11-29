2f9617a12bbef483f25a403fb891ac9e7c42a63e
PrettyScript <JessicaDelcy@gmail.com>
2022-11-29 11:51:52 -0600

    started ui for capstone project

--- day_31_capstone_flash_card/main.py
+++ day_31_capstone_flash_card/main.py
@@ -1,2 +1,15 @@
 BACKGROUND_COLOR = "#B1DDC6"
 
+from tkinter import *
+
+window = Tk()
+window.title("Flashy")
+
+
+canvas = Canvas(width=800, height=530, background=BACKGROUND_COLOR)
+front_card_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 250, front_card_img)
+canvas.grid(column=1, row=0)
+
+
+window.mainloop()
