2e67f87cb6ad488c1cf1eb9aeb26932431468b39
PrettyScript <JessicaDelcy@gmail.com>
2022-12-03 14:56:45 -0600

    added flipping cards functionality

--- day_31_capstone_flash_card/main.py
+++ day_31_capstone_flash_card/main.py
@@ -10,23 +10,36 @@ data_file = pandas.read_csv("./data/french_words.csv")
 # words = data_file.set_index("French")["English"].to_dict()
 # random_french_word = choice(french_words)
 to_learn = data_file.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = choice(to_learn)
     random_card = current_card["French"]
-    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=random_card)
+    canvas.itemconfig(card_background, card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
@@ -39,6 +52,10 @@ unknown_button.grid(row=1, column=0)
 check_img = PhotoImage(file="./images/right.png")
 known_button = Button(image=check_img, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
+
+new_image = PhotoImage(file="./images/card_back.png")
+old_image = PhotoImage(file="./images/card_front.png")
+
 
 next_card()
 
