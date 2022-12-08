a419d8d859c917dc12837914e26b5d45dc90d849
PrettyScript <JessicaDelcy@gmail.com>
2022-12-08 00:39:15 -0600

    imported pandas:

--- day_32_automated_birthday_wisher/main.py
+++ day_32_automated_birthday_wisher/main.py
@@ -1,15 +1,27 @@
 ##################### Hard Starting Project ######################
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+# 1. Update the birthdays.csv with your friends & family's details.
+# HINT: Make sure one of the entries matches today's date for testing purposes.
 
 # 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
+# HINT 1: Only the month and day matter.
 # HINT 2: You could create a dictionary from birthdays.csv that looks like this:
 # birthdays_dict = {
 #     (month, day): data_row
 # }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+import datetime as dt
+import pandas
+
+now = dt.datetime.now()
+day = now.day
+month = now.month
+todays_date = f"{month}/{day}"
+# print(todays_date)
+
+data = pandas.read_csv("birthdays.csv")
+print(data)
+
+# HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
@@ -17,6 +29,3 @@
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-
-
-
