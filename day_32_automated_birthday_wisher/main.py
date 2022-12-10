43f1f1ec88be4b02f22ba9c87efd30b6317c0f5e
PrettyScript <JessicaDelcy@gmail.com>
2022-12-10 10:45:58 -0600

    completed day 32

--- day_32_automated_birthday_wisher/main.py
+++ day_32_automated_birthday_wisher/main.py
@@ -1,31 +1,36 @@
-##################### Hard Starting Project ######################
+##################### Normal Starting Project ######################
 
 # 1. Update the birthdays.csv with your friends & family's details.
-# HINT: Make sure one of the entries matches today's date for testing purposes.
+# HINT: Make sure one of the entries matches today's date for testing purposes. e.g.
+# name,email,year,month,day
+# YourName,your_own@email.com,today_year,today_month,today_day
+from datetime import datetime
+import pandas
+from random import randint
+import smtplib
+
+MY_EMAIL = "jessicatestingwithpython@gmail.com"
+MY_PASSWORD = "ljshdhjnad()@"
 
 # 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter.
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-import datetime as dt
-import pandas
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datetime.now()
-day = now.day
-month = now.month
-todays_date = f"{month}/{day}"
-# print(todays_date)
-
+# HINT 2: Use pandas to read the birthdays.csv
 data = pandas.read_csv("birthdays.csv")
-print(data)
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+# print(birthdays_dict)
 
-# HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"./letter_templates/letter_{randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject: Happy Birthday\n\n{contents}"
+        )
