205a9f3b9e9dba014f3a3ad89a160cc648bd80f5
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2023-01-12 23:39:05 -0600

    started on data manager class

--- day_39_capstone_flight_finder/data_manager.py
+++ day_39_capstone_flight_finder/data_manager.py
@@ -1,3 +1,25 @@
+import config
+import requests
+from pprint import pprint
+
+USERNAME = config.sheety_username
+PROJECT_NAME = "flightDeals"
+SHEET_NAME = "prices"
+
+SHEETY_TOKEN = config.sheety_api_token
+sheety_endpoint = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
+
+sheety_headers = {
+    "Authorization": SHEETY_TOKEN
+}
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        pass
+    
+    def get_data(self):
+        response = requests.get(url=sheety_endpoint, headers=sheety_headers)
+        data = response.json()
+        # print(response.text)
+        pprint(data)
